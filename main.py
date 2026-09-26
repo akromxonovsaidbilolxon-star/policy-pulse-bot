b748e1f4f43982c32278ea92928c3a0f9662ee98
akromxonovsaidbilolxon-star <akromxonovsaidbilolxon@gmail.com>
2026-09-26 06:02:45 -0400

    Update model version from gemini-2.0 to gemini-3.8

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def extract_truck_data_with_ai(raw_text):
     """
     try:
         response = client.models.generate_content(
-            model='gemini-2.0-flash',
+            model='model='gemini-3.8-flash',
             contents=prompt,
         )
         clean_text = response.text.replace("```json", "").replace("```", "").strip()
